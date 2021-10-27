efd3a7e724056ce02bc774bb7dd02966fa3f3ef9
SabinKh <91114252+SabinKh@users.noreply.github.com>
2021-10-27 04:25:31 +0400

    create custom list

---
+++ CreateList/Collections/MyList.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreateList.Collections
+{
+    public class MyList<T>
+    {
+        private T[] arr;
+        public int count { get; set; }
+        public MyList()
+        {
+            arr = new T[10];
+        }
+
+        public void AddRange(IEnumerable<T> list)
+        {
+            foreach (var item in list)
+            {
+                Add(item);
+            }
+        }
+
+        public void Add(T obj)
+        {
+            if ((count != 0) && (count % 10 == 0))
+            {
+                ArrayResize();
+            }
+            arr[count] = obj;
+            count++;
+        }
+
+        private void ArrayResize()
+        {
+            Array.Resize(ref arr, arr.Length + 10);
+        }
+
+        public void Clear()
+        {
+            arr = new T[10];
+            count = 0;
+        }
+
+        public T Find(Predicate<T> func)
+        {
+            foreach (var item in arr)
+            {
+                if (func(item))
+                {
+                    return item;
+                }
+            }
+            return default;
+        }
+
+        public bool Contains(T obj)
+        {
+            foreach (var item in arr)
+            {
+                if (Comparer<T>.Default.Compare(item, obj) == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool Remove(T obj)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (Comparer<T>.Default.Compare(arr[i], obj) == 0)
+                {
+                    arr[i] = default;
+                    count--;
+                    return true;
+
+                }
+
+            }
+            return false;
+        }
+        public int LastIndexOf(T obj)
+        {
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                if (Comparer<T>.Default.Compare(arr[i], obj) == 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+
+        public int IndexOf(T obj)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (Comparer<T>.Default.Compare(arr[i], obj) == 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ CreateList/Program.cs
@@ -0,0 +1,27 @@
+﻿using CreateList.Collections;
+using System;
+
+namespace CreateList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> list = new MyList<string>();
+            list.Add("Hello1");
+            list.Add("Hello2");
+            list.Add("Hello3");
+            list.Add("Hello4");
+            list.Add("Hello5");
+            list.Add("Hello6");
+            list.Add("Hello7");
+            list.Add("Hello8");
+            list.Add("Hello9");
+            list.Add("Hello10");
+            list.Add("Hello11");
+            MyList<int> list2 = new MyList<int>();
+            Console.WriteLine(list2.Find(i=>i==100));
+
+        }
+    }
+}
